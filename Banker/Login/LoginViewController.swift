1f9e8c4953dc3f23877c01842152a49d1d554bc3
Artem <mrdmitrievv@yandex.ru>
2023-01-24 18:38:34 +0300

    Create signInButton in LoginVC

--- Banker/Login/LoginViewController.swift
+++ Banker/Login/LoginViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class LoginViewController: UIViewController {
 
     let loginView = LoginView()
+    let signInButton = UIButton(type: .system)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,18 +25,41 @@ extension LoginViewController {
     
     private func style() {
         view.translatesAutoresizingMaskIntoConstraints = false
+        
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        signInButton.configuration = .filled()
+        signInButton.configuration?.imagePadding = 8 // for indicator spacing
+        signInButton.setTitle("Sign in", for: [])
+        signInButton.addTarget(self, action: #selector(signInButtonDidPressed), for: .primaryActionTriggered)
+        
     }
     
     private func layout() {
         view.addSubview(loginView)
+        view.addSubview(signInButton)
         
+        // LoginView
         NSLayoutConstraint.activate([
             loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
         ])
         
+        // signInButton
+        NSLayoutConstraint.activate([
+            signInButton.topAnchor.constraint(equalToSystemSpacingBelow: loginView.bottomAnchor, multiplier: 2),
+            signInButton.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            signInButton.trailingAnchor.constraint(equalTo: loginView.trailingAnchor)
+        ])
+        
     }
-    
 }
+
+extension LoginViewController {
+    @objc func signInButtonDidPressed(sender: UIButton) {
+        print("signIn did pressed")
+    }
+}
+
+
 
