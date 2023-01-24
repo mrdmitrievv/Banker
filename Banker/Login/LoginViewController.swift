9b33426d1fbc3e76135f12e8c86c23f275f9a238
Artem <mrdmitrievv@yandex.ru>
2023-01-24 19:21:06 +0300

    Add logic into signIn button

--- Banker/Login/LoginViewController.swift
+++ Banker/Login/LoginViewController.swift
@@ -11,6 +11,15 @@ class LoginViewController: UIViewController {
 
     let loginView = LoginView()
     let signInButton = UIButton(type: .system)
+    let errorLabel = UILabel()
+    
+    var username: String? {
+        return loginView.usernameTextField.text
+    }
+    
+    var password: String? {
+        return loginView.passwordTextField.text
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,32 +41,65 @@ extension LoginViewController {
         signInButton.setTitle("Sign in", for: [])
         signInButton.addTarget(self, action: #selector(signInButtonDidPressed), for: .primaryActionTriggered)
         
+        errorLabel.translatesAutoresizingMaskIntoConstraints = false
+        errorLabel.textColor = .systemRed
+        errorLabel.numberOfLines = 0
+        errorLabel.textAlignment = .center
+        errorLabel.isHidden = false
     }
     
     private func layout() {
         view.addSubview(loginView)
         view.addSubview(signInButton)
+        view.addSubview(errorLabel)
         
-        // LoginView
         NSLayoutConstraint.activate([
             loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
         ])
         
-        // signInButton
         NSLayoutConstraint.activate([
             signInButton.topAnchor.constraint(equalToSystemSpacingBelow: loginView.bottomAnchor, multiplier: 2),
             signInButton.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
             signInButton.trailingAnchor.constraint(equalTo: loginView.trailingAnchor)
         ])
         
+        NSLayoutConstraint.activate([
+            errorLabel.topAnchor.constraint(equalToSystemSpacingBelow: signInButton.bottomAnchor, multiplier: 2),
+            errorLabel.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            errorLabel.trailingAnchor.constraint(equalTo: loginView.trailingAnchor)
+        ])
     }
 }
 
 extension LoginViewController {
     @objc func signInButtonDidPressed(sender: UIButton) {
-        print("signIn did pressed")
+        errorLabel.isHidden = true
+        login()
+    }
+    
+    private func login() {
+        guard let username = username, let password = password else {
+            assertionFailure("Username / password can not be nil")
+            return
+        }
+        
+        if username.isEmpty || password.isEmpty {
+            showFailure(withMessage: "Username / password can not be blank")
+            return
+        }
+        
+        if username == "Tim" && password == "Qqqqqq" {
+            signInButton.configuration?.showsActivityIndicator = true
+        } else {
+            showFailure(withMessage: "Incorrect username / password")
+        }
+    }
+    
+    private func showFailure(withMessage message: String) {
+        errorLabel.text = message
+        errorLabel.isHidden = false
     }
 }
 
